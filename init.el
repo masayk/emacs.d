40c62e6a16cd7b545d037f56c4d747ca6f1d5db3
masayk <>
2024-09-07 12:27:40 +0900

    add org-roam

--- init.el
+++ init.el
@@ -130,6 +130,20 @@
 ;; org-fold
 (load "~/.emacs.d/elisp/org-fold.el")
 
+;; org-roam
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/org/org-roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode))
+
 
 ;;
 ;; Command
